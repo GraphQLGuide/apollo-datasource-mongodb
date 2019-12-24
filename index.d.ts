3f76598908e60c215f7e219e292d04122c81a022
Yoonho Go <rhdbsgh0629@naver.com>
2019-12-24 17:01:50 +0900

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,15 @@ declare module 'apollo-datasource-mongodb' {
   import mongoose, { Document, Model } from 'mongoose'
   import { DataSource, DataSourceConfig } from 'apollo-datasource'
 
-  type Options = { ttl: number }
+  export {
+    DataSourceConfig
+  }
+
+  interface Options { ttl: number }
 
   export abstract class MongoDataSource<T extends Document, TContext = any> extends DataSource {
-    collection: mongoose.Collection | Model<T>
+    collection: mongoose.Collection
+    model?: Model<T>
     constructor(collection: mongoose.Collection | Model<T>)
     public initialize(config: DataSourceConfig<TContext>): void
     protected findOneById(id: string, { ttl }?: Options): T | null
