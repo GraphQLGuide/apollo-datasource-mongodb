1649aa8e09638dfd515cd1bd460e17855b01db57
Huy Hoang <boydaihungst@gmail.com>
2020-09-04 21:36:59 +0700

    Fix duplicate ObjectId keys

--- src/cache.js
+++ src/cache.js
@@ -1,7 +1,7 @@
 import DataLoader from 'dataloader'
 
 import { getCollection, isModel } from './helpers'
-
+import { ObjectId } from 'bson'
 // https://github.com/graphql/dataloader#batch-function
 const orderDocs = ids => docs => {
   const idMap = {}
@@ -29,14 +29,15 @@ export const createCachingMethods = ({ collection, model, cache }) => {
 
   const methods = {
     findOneById: async (id, { ttl } = {}) => {
-      const key = cachePrefix + id
+      const _id = id instanceof ObjectId ? id.toHexString() : id
+      const key = cachePrefix + _id
 
       const cacheDoc = await cache.get(key)
       if (cacheDoc) {
         return JSON.parse(cacheDoc)
       }
 
-      const doc = await loader.load(id)
+      const doc = await loader.load(_id)
       if (Number.isInteger(ttl)) {
         // https://github.com/apollographql/apollo-server/tree/master/packages/apollo-server-caching#apollo-server-caching
         cache.set(key, JSON.stringify(doc), { ttl })
