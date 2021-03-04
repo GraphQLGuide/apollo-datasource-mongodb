dd75571d81796e8f7ae5601ae06ee941f5b49716
Ryan Goree <goree.ryan@gmail.com>
2021-03-04 07:44:46 -0600

    Add findByFields tests

--- src/__tests__/cache.test.js
+++ src/__tests__/cache.test.js
@@ -9,15 +9,19 @@ const hexId = 'aaaa0000bbbb0000cccc0000'
 
 const docs = {
   one: {
-    _id: ObjectId(hexId)
+    _id: ObjectId(hexId),
+    foo: 'bar',
+    tags: ['foo', 'bar']
   },
   two: {
-    _id: ObjectId()
+    _id: ObjectId(),
+    foo: 'bar'
   }
 }
 
 const stringDoc = {
-  _id: 's2QBCnv6fXv5YbjAP'
+  _id: 's2QBCnv6fXv5YbjAP',
+  tags: ['bar', 'baz']
 }
 
 const collectionName = 'test'
@@ -31,30 +35,55 @@ describe('createCachingMethods', () => {
   beforeEach(() => {
     collection = {
       collectionName,
-      find: jest.fn(({ _id: { $in: ids } }) => ({
-        toArray: () =>
-          new Promise(resolve => {
-            setTimeout(
-              () =>
-                resolve(
-                  ids.map(id => {
-                    if (id === stringDoc._id) {
-                      return stringDoc
-                    }
-
-                    if (id.equals(docs.one._id)) {
-                      return docs.one
-                    }
-
-                    if (id.equals(docs.two._id)) {
-                      return docs.two
-                    }
-                  })
-                ),
-              0
-            )
-          })
-      }))
+      find: jest.fn(filter => {
+        return {
+          toArray: () =>
+            new Promise(resolve => {
+              setTimeout(
+                () =>
+                  resolve(
+                    [docs.one, docs.two, stringDoc].filter(doc => {
+                      for (const orFilter of filter.$or || [filter]) {
+                        for (const field in orFilter) {
+                          if (field === '_id') {
+                            for (const id of orFilter._id.$in) {
+                              if (id.equals && !id.equals(doc._id)) {
+                                break
+                              } else if (
+                                doc._id.equals &&
+                                !doc._id.equals(id)
+                              ) {
+                                break
+                              } else if (
+                                !id.equals &&
+                                !doc._id.equals &&
+                                id !== doc._id
+                              ) {
+                                break
+                              }
+                            }
+                          } else if (Array.isArray(doc[field])) {
+                            for (const value of orFilter[field].$in) {
+                              if (!doc[field].includes(value)) {
+                                break
+                              }
+                            }
+                          } else if (
+                            !orFilter[field].$in.includes(doc[field])
+                          ) {
+                            break
+                          }
+                        }
+                        return true
+                      }
+                      return false
+                    })
+                  ),
+                0
+              )
+            })
+        }
+      })
     }
 
     cache = new InMemoryLRUCache()
@@ -65,10 +94,11 @@ describe('createCachingMethods', () => {
   it('adds the right methods', () => {
     expect(api.findOneById).toBeDefined()
     expect(api.findManyByIds).toBeDefined()
+    expect(api.findByFields).toBeDefined()
     expect(api.deleteFromCacheById).toBeDefined()
   })
 
-  it('finds one', async () => {
+  it('finds one with ObjectId', async () => {
     const doc = await api.findOneById(docs.one._id)
     expect(doc).toBe(docs.one)
     expect(collection.find.mock.calls.length).toBe(1)
@@ -85,6 +115,62 @@ describe('createCachingMethods', () => {
 
     expect(foundDocs[0]).toBe(docs.one)
     expect(foundDocs[1]).toBe(docs.two)
+
+    expect(collection.find.mock.calls.length).toBe(1)
+  })
+
+  it('finds by field', async () => {
+    const foundDocs = await api.findByFields({ foo: 'bar' })
+
+    expect(foundDocs[0]).toBe(docs.one)
+    expect(foundDocs[1]).toBe(docs.two)
+    expect(foundDocs.length).toBe(2)
+
+    expect(collection.find.mock.calls.length).toBe(1)
+  })
+
+  it('finds by array field', async () => {
+    const foundDocs = await api.findByFields({ tags: 'bar' })
+
+    expect(foundDocs[0]).toBe(docs.one)
+    expect(foundDocs[1]).toBe(stringDoc)
+    expect(foundDocs.length).toBe(2)
+
+    expect(collection.find.mock.calls.length).toBe(1)
+  })
+
+  it('finds by mutiple fields', async () => {
+    const foundDocs = await api.findByFields({
+      tags: ['foo', 'bar'],
+      foo: 'bar'
+    })
+
+    expect(foundDocs[0]).toBe(docs.one)
+    expect(foundDocs.length).toBe(1)
+
+    expect(collection.find.mock.calls.length).toBe(1)
+  })
+
+  it(`doesn't mix filters of pending calls for different fields`, async () => {
+    const pendingDocs1 = api.findByFields({ foo: 'bar' })
+    const pendingDocs2 = api.findByFields({ tags: 'baz' })
+    const [foundDocs1, foundDocs2] = await Promise.all([
+      pendingDocs1,
+      pendingDocs2
+    ])
+
+    expect(foundDocs1[0]).toBe(docs.one)
+    expect(foundDocs1[1]).toBe(docs.two)
+    expect(foundDocs1.length).toBe(2)
+    expect(foundDocs2[0]).toBe(stringDoc)
+    expect(foundDocs2.length).toBe(1)
+
+    expect(collection.find.mock.calls.length).toBe(1)
+  })
+
+  it(`caches each value individually when finding by a single field`, async () => {
+    await api.findByFields({ tags: ['foo', 'baz'] })
+    await api.findByFields({ tags: 'foo' })
 
     expect(collection.find.mock.calls.length).toBe(1)
   })
