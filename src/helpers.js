abf6dd72fccb5a53b5d93f4cb7ba235092aea190
SleepWalker <dev@udf.su>
2021-03-20 16:41:41 +0200

    fix: support class-based mongoose models too (closes #51)

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,14 @@
 const TYPEOF_COLLECTION = 'object'
 
-export const isModel = x => Boolean(x && x.name === 'model')
+export const isModel = x =>
+  Boolean(
+    typeof x === 'function' &&
+      x.prototype &&
+      /**
+       * @see https://github.com/Automattic/mongoose/blob/b4e0ae52a57b886bc7046d38332ce3b38a2f9acd/lib/model.js#L116
+       */
+      x.prototype.$isMongooseModelPrototype
+  )
 
 export const isCollectionOrModel = x =>
   Boolean(x && (typeof x === TYPEOF_COLLECTION || isModel(x)))
